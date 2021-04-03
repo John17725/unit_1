a676c6f3638725854089ec562d0db69f22f5d721
John17725 <barthjoss@hotmail.es>
2021-04-03 13:29:45 -0600

    change styles index

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 addEventListener('DOMContentLoaded',()=>{
 
-	const arr_img = ['img/img_01.jpg','img/img_02.jpg','img/img_03.jpg','img/img_04.jpg','img/img_05.jpg',];
+	const arr_img = ['img/img_01.jpg','img/img_02.jpg','img/img_03.jpg','img/img_04.jpg','img/img_05.jpg','img/img_06.jpg','img/img_07.jpg','img/img_08.jpg','img/img_09.jpg','img/img_10.jpg','img/img_11.jpg'];
 	let i = 1
 	const img1 = document.querySelector('#img1')
 	const img2 = document.querySelector('#img2')
