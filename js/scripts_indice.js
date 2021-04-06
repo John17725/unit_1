956dee0adfcf4c33f277afc1d03623d07fe3500e
John17725 <barthjoss@hotmail.es>
2021-04-05 21:11:38 -0500

    Indice advance 8

--- js/scripts_indice.js
+++ js/scripts_indice.js
@@ -4,11 +4,13 @@ addEventListener('DOMContentLoaded',()=>{
 	const arr_img = ['img/img_12.jpg','img/img_13.jpg','img/img_14.jpg','img/img_15.jpg','img/img_16.jpg','img/img_17.jpg'];
 	const arr_img2 = ['img/img_desafio_1.jpg','img/img_desafio_2.jpg'];
 	const arr_img3 = ['img/img_geo_1.jpg','img/img_geo_2.jpg','img/img_geo_3.jpg'];
-	const arr_img4 = ['img/img_concurrencia_1.png','img/img_concurrencia_1.png'];
+	const arr_img4 = ['img/img_concurrencia_1.png','img/img_concurrencia_2.jpg','img/img_concurrencia_3.jpg'];
+	const arr_img5 = ['img/img_desafio_1.jpg','img/img_desafio_2.jpg'];
 	let i = 1
 	let j = 1
 	let k = 1
 	let l = 1
+	let o = 1
 	const img1 = document.querySelector('#img1')
 	const img2 = document.querySelector('#img2')
 	const img3 = document.querySelector('#img3')
@@ -17,6 +19,8 @@ addEventListener('DOMContentLoaded',()=>{
 	const img6 = document.querySelector('#img6')
 	const img7 = document.querySelector('#img7')
 	const img8 = document.querySelector('#img8')
+	const img9 = document.querySelector('#img9')
+	const img10 = document.querySelector('#img10')
 
 	
 
@@ -24,6 +28,7 @@ addEventListener('DOMContentLoaded',()=>{
 	img3.src = arr_img2[0]
 	img5.src = arr_img3[0]
 	img7.src = arr_img4[0]
+	img9.src = arr_img5[0]
 
 
 	const slideshow = () => {
@@ -74,10 +79,23 @@ addEventListener('DOMContentLoaded',()=>{
 			img8.classList.remove('active')
 		},0)
 	}
+	const slideshow5 = () => {
+		img10.src = arr_img5[o]
+		img10.classList.add('active')
+		o++
+		if (o == arr_img5.length) {
+			o = 0
+		}
+		setTimeout(() => {
+			img9.src = img10.scr
+			img10.classList.remove('active')
+		},0)
+	}
 	setInterval(slideshow, 4000)
 	setInterval(slideshow2, 4000)
 	setInterval(slideshow3, 4000)
 	setInterval(slideshow4, 4000)
+	setInterval(slideshow5, 4000)
 })
 
 
--- js/scripts_indice_slides.js
+++ js/scripts_indice_slides.js
@@ -1,34 +1,84 @@
 let slider = document.querySelector(".slider-contenedor")
 let sliderIndividual = document.querySelectorAll(".contenido-slider")
 let pos = 1;
+let pos2 = 6;
 let width = sliderIndividual[0].clientWidth;
 let btnDer = document.querySelector("#btn-der")
-
-
-
+let btnIzq = document.querySelector("#btn-izq")
+let div = document.querySelector("#content-btn-izq")
+let vecespresionadobtnizq = 0;
 window.addEventListener("resize", function(){
     width = sliderIndividual[0].clientWidth;
 })
 
+btnIzq.addEventListener('click', function(){
+	vecespresionadobtnizq++;
+	if (vecespresionadobtnizq==1  ) {
+		ToslidesDer1_2();
+		pos = 6;
+		pos2++;
+	}if (vecespresionadobtnizq == 2) {
+		ToslidesDer1_1();
+		pos = 1;
+		pos2 = 6;
+		vecespresionadobtnizq=0;
+	}
+	console.log("POS:",pos)
+	console.log("POS2:",pos2)
+})
 
 btnDer.addEventListener('click', function(){
-	if (pos==5) {
-		alert("Has llegado al final")
-		slider.style.transform = "translate("+(-width*0)+"px)";
-		slider.style.transition = "transform .8s";
-		pos = 0
-	}if (pos<5) {
+	if (pos2 == 6) {
+		mostrarbtnizq();
+	}
+	if (pos>=6 && pos2 <14) {
+		ocultarbtnizq();
+		slidesDer1_2();
+		console.log(pos2)
+		if (pos2 == 14) {
+			mostrarbtnizq();
+			vecespresionadobtnizq=0;
+
+		}
+	}
+
+	if (pos<=5) {
 		console.log(pos)
 		slidesDer()
+		if (pos>1 && pos<=5) {
+			ocultarbtnizq();
+		}
 
 	}
 })
 
 
+function mostrarbtnizq(){
+	div.style.display = 'block'
+
+}
+function ocultarbtnizq(){
+	div.style.display = "none";
+}
 
 function slidesDer(){
+	console.log("POS DESDE BTNDER:",pos)
     slider.style.transform = "translate("+(-width*pos)+"px)";
     slider.style.transition = "transform .8s";
     pos++;
 }
+function slidesDer1_2(){
+    slider.style.transform = "translate("+(-width*pos2)+"px)";
+    slider.style.transition = "transform .8s";
+    pos2++;
+}
+function ToslidesDer1_2(){
+    slider.style.transform = "translate("+(-width*6)+"px)";
+    slider.style.transition = "transform .8s";
+}
+function ToslidesDer1_1(){
+    slider.style.transform = "translate("+(-width*0)+"px)";
+    slider.style.transition = "transform .8s";
+}
+
 
